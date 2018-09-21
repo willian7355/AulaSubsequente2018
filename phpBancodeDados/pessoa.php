30a3c639955579e7e1d1a739500f951b53ffa88f
rafaelflorindo <rafaelflorindo@hotmail.com>
2018-09-21 13:56:31 -0300

    Create pessoa.php

---
+++ phpBancodeDados/pessoa.php
@@ -0,0 +1,10 @@
+<?php
+include 'conexao.php';
+
+?>
+<form action = "" method = "POST">
+<label>Nome: </label><input type = "text" name="nome"><br />
+<label>Nome: </label><input type = "text" name="nome"><br />
+<label>Nome: </label><input type = "text" name="nome"><br />
+<label>Nome: </label><input type = "text" name="nome"><br />
+</form>
