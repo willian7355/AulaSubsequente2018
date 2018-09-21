8a88598c3e1cf896c2b01ddb07019c8415160e3d
rafaelflorindo <rafaelflorindo@hotmail.com>
2018-09-21 13:23:24 -0300

    Update conexao.php

--- phpBancodeDados/conexao.php
+++ phpBancodeDados/conexao.php
@@ -13,5 +13,16 @@
 	//mysqli orientada, funcina com a API do mysql 6
 	//$conectar = mysqli_connect($local, $usuario,$senha,$baseDados);
 	
-	$conectar = new mysqli ($local, $usuario,$senha,$baseDados);	
+	$conectar = new mysqli ($local, $usuario,$senha,$baseDados);
+	
+	//*************Realizando a validação da conexão com o banco de dados**************************
+	/*A função mysqli_connect_errno() irá retornar o número do código de erro da ultima chamada a função mysqli_connect(). Se não houve erro, esta função irá retornar zero. 
+	A função connect_error irá retornar uma string representando o ultimo erro que aconteceu com a ultima chamada a função mysqli_connect(). Se não houve erro, esta função irá retornar uma string vazia. 	
+	*/
+	if ($mysqli->connect_errno) { 
+		echo "Houve um erro na tentativa de conexão com MySQL:
+			Código do erro: (" . $mysqli->connect_errno . ") Motivo: " . $mysqli->connect_error;
+	}
+	echo "A sua conexão foi realizada com sucesso via " . $mysqli->host_info;
+
 ?> 
